504934a80fd6ac1b6cf1c7810c577cc0409a054e
Gery Oviedo <139388480+gpinedaoviedo@users.noreply.github.com>
2026-07-18 01:05:58 -0400

    Configure production API

--- frontend/js/api/dictionaryApi.js
+++ frontend/js/api/dictionaryApi.js
@@ -1,7 +1,7 @@
 export async function getWordData(data) {
     const API_URL = location.hostname === "localhost" || "127.0.0.1" 
         ? "http://127.0.0.1:8000"
-        : "https://DictionaryOnline.es.onrender.com"
+        : "https://dictionary-online.onrender.com"
 
     const response = await fetch(`${API_URL}/api/search/en/definition?word=${data}`);
         
